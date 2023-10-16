011a64c3957093511c72a9aa0dbb71f01dad7329
dayshah <dhyeyhshah@gmail.com>
2023-10-16 01:36:26 -0400

    i wish we had a diff way to interview

--- some_hards.cpp
+++ some_hards.cpp
@@ -2,14 +2,6 @@
 #include <iostream>
 #include <map>
 
-
-struct Person {
-    std::string name;
-    Person(const std::string& name, int age):
-        name(name) 
-    {}
-};
-
 int longestNonDecreasing(const std::vector<int>& nums1, const std::vector<int>& nums2) {
     int dp1 = 1;
     int dp1Future = 1;
@@ -30,11 +22,45 @@ int longestNonDecreasing(const std::vector<int>& nums1, const std::vector<int>& 
     return res;
 }
 
-int goodBinaryStrings(int minLength, int maxLength, int oneGroup, int twoGroup) {
-    
+int goodBinaryStrings(int minLength, int maxLength, int oneGroup, int zeroGroup) {
+    int mod = (int)std::powf(10,9) + 7;
+    int dp[maxLength];
+    dp[0] = 1;
+    int res = 0;
+    for (int i = 1; i < maxLength; ++i) {
+        if (i >= oneGroup) dp[i] += dp[i - oneGroup];
+        if (i >= zeroGroup) dp[i] += dp[i - zeroGroup];
+        if (i >= minLength) res += static_cast<int>(dp[i] % mod);
+    }
+    return res;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    ~TreeNode() { delete left; delete right; }
+};
+
+int maxPathSum(TreeNode* root) {
+    int maxSum = -INFINITY;
+    std::function<int(TreeNode*)> func;
+    func = [&maxSum, &func](TreeNode* node){
+        if (!node) return 0;
+        int leftGain = std::max(func(node->left), 0);
+        int rightGain = std::max(func(node->right), 0);
+        int gain = leftGain + rightGain + node->val;
+        maxSum = std::max(maxSum, gain);
+        return gain;
+    };
+    func(root);
+    return maxSum;
 }
 
 
 int main() {
-    
+    std::cout << goodBinaryStrings(3,3,1,1) << std::endl;
 }
