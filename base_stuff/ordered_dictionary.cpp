4f615e3c2dbede9b644e786559b245894037ff44
Dhyey Shah <85444498+dayshah@users.noreply.github.com>
2024-03-05 04:00:27 +0000

    want to avoid key copy

--- base_stuff/ordered_dictionary.cpp
+++ base_stuff/ordered_dictionary.cpp
@@ -1,43 +1,43 @@
 
+#include <functional>
 #include <iostream>
 #include <list>
 #include <ostream>
 #include <string>
-#include <unordered_map>
+#include <unordered_map> 
 
 template <typename K, typename V>
 class OrderedDictionary {
-    //typedef std::unordered_map<K,V>::iterator map_iterator;
-    typedef std::list<const K*>::iterator list_iterator;
+    typedef std::list<std::pair<K,V>>::const_iterator list_iterator;
+    typedef std::unordered_map<K,list_iterator>::const_iterator map_iterator;
 
     private:
-    std::unordered_map<K, V> map;
-    std::list<const K*> list;
-    std::unordered_map<const K*, list_iterator> iteratorMap;
+    std::unordered_map<K, list_iterator> map;
+    std::list<std::pair<K,V>> list;
 
     public:
     void push_back(const std::pair<K,V>& pair) {
-        map.insert(pair);
-        const K* key_ptr = &(map.find(pair.first)->first);
-        list.push_back(key_ptr);
-        iteratorMap.insert(key_ptr, list.end()-1);
+        list.push_back(pair);
+        map.insert({pair.first, std::prev(list.end())});
     }
     void push_back(std::pair<K,V>&& pair) {
-        map.insert(pair);
-        const K* key_ptr = &(map.find(pair.first)->first);
-        list.push_back(key_ptr);
-        //iteratorMap.insert(key_ptr, list.end()-1);
+        list.push_back({pair.first, std::move(pair.second)});
+        map.insert({std::move(pair.first), std::prev(list.end())});
     }
     bool remove_key(const K& key) {
-        const auto& foundIter = map.find(key);
-        const K* key_ptr = &(foundIter->first);
-        if (foundIter != map.end()) {
-            list.erase(iteratorMap.at(key_ptr));
-            iteratorMap.erase(key_ptr);
-            map.erase(foundIter);
+        const auto& map_iter = map.find(key);
+        if (map_iter != map.end()) {
+            list.erase(map_iter->second);
+            map.erase(map_iter);
             return true;
         }
         return false;
+    }
+    const V& get_back_value() const {
+        return list.back().second;
+    }
+    const V& get_value_for_key(const K& key) const {
+        return map.at(key)->second;
     }
     void printSizes() {
         std::cout << "list size: " << list.size() << std::endl;
@@ -49,6 +49,9 @@ int main() {
     OrderedDictionary<int, std::string> test{};
     test.push_back({1, "hi"});
     test.printSizes();
+    std::cout << "\n" << test.get_back_value() << "\n";
+    std::cout << test.get_value_for_key(1) << "\n\n";
     test.remove_key(1);
+    test.printSizes();
     return 0;
 }
